51a2707fc269315d8853500426951f2927e198ad
Danilo Alves da Silva Sales <danilo.alves@pixeon.com>
2022-08-05 18:18:01 -0300

    tests with stack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Stack from './data-structures/stack';
+import { parenthesesChecker } from './others/balanced-symbols';
+import { baseConverter, decimalToBinary } from './others/base-converter';
+import { hanoi, hanoiStack } from './others/hanoi';
 
 const stack = new Stack();
 stack.push(5);
@@ -9,3 +12,21 @@ stack.pop();
 console.log(stack.size())
 stack.clear();
 console.log(stack.isEmpty());
+
+console.log(decimalToBinary(10));
+console.log(decimalToBinary(233));
+console.log(decimalToBinary(1000));
+console.log(baseConverter(100345, 2));
+console.log(baseConverter(100345, 8));
+console.log(baseConverter(100345, 16));
+console.log(baseConverter(100345, 35));
+
+
+console.log('{([])}', parenthesesChecker('{([])}')); // true
+console.log('{{([][])}()}', parenthesesChecker('{{([][])}()}')); // true
+console.log('[{()]', parenthesesChecker('[{()]')); // false
+
+
+console.log(hanoiStack(3));
+
+console.log(hanoi(3, 'source', 'helper', 'dest'));
